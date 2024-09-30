d301a8baea508a457e32172e02a4ab28a3ddb77b
Tommy's Macbook Pro(M3 Pro) <zwt03712@gmail.com>
2024-09-30 10:03:37 +1000

    update redis approach

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
+import os
 from fastapi import FastAPI
 import redis
 
 app = FastAPI()
 
-redis_url = "redis://:4okRPRToSOjkgIlmcoQMoMorn06GuaRgMUmPq55SqC6VoWcjMvXjUeQQXz0lyOH4@194.195.254.249:5432/0"
+redis_url = (
+    os.getenv("REDIS_URL")
+    or "redis://:4okRPRToSOjkgIlmcoQMoMorn06GuaRgMUmPq55SqC6VoWcjMvXjUeQQXz0lyOH4@194.195.254.249:5432/0"
+)
 redis_client = redis.from_url(redis_url)
 
 
 @app.post("/add-task/")
 def add_task(data: dict):
-    # 将任务推送到 Redis 列表
     redis_client.lpush("task_queue", data["task_data"])
     return {"status": "Task added to Redis"}
